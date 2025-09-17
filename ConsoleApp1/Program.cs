35425e1b8321a2c4b4a5741b73f9019d91e0cc07
Moonlight-twj <2338144150@qq.com>
2025-09-17 23:46:28 +0800

    Assignment

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("班级；计科2401，姓名；庹雪怡，序号；30");
+        }
+    }
+}
