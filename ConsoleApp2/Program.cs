ca1d89be1a7d8e763ebadd6eca89c632da4a0b44
Moonlight-twj <2338144150@qq.com>
2025-09-17 16:28:31 +0800

    Add initial projects for ConsoleApp2-5 and day1 file

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"1.fioat的最小值:{float.MinValue}");
+            Console.WriteLine($"2. 3.3等于5.5：{double.Equals(3.3, 5.5)}");
+            decimal result = decimal.Add(3.3m, 5.5m);
+            Console.WriteLine($"3. 3.3+5.5={result}");
+            Console.WriteLine("\n按任意键退出程序");
+            Console.ReadKey( );
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The min char:{0}",(int)(char .MinValue ));
+            Console.WriteLine("The max char:{0}",(int)(char.MaxValue));
+            Console.WriteLine("'2'is letter?{0}", char.IsLetter('2'));
+            string testString = "13";
+            Console.WriteLine("'13'is string?{0}",testString is string );
+            Console.WriteLine("'D'is lower?{0}", char.IsLower('D'));
+            Console.WriteLine("\"Tom\"==\"Tomy\"?{0}", string.Equals("Tom", "Tony"));
+            Console.WriteLine("{0}", string.Concat("我是", "用工学习者"));
+        }
+    }
+}
