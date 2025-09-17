317ac69fdbe5651001bd99b1076315274e970b98
Moonlight-twj <2338144150@qq.com>
2025-09-17 21:16:39 +0800

    filler content

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            year=Convert .ToInt32 (Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("{0}是闰年，year");
+            }
+            else
+                Console.WriteLine("{0}不是闰年", year);
+        }
+    }
+}
