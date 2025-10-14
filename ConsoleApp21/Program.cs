a09f71467410f48dbe20226c14c775aab0c6e9ee
Moonlight-twj <2338144150@qq.com>
2025-10-14 22:22:09 +0800

    schooiwork

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Teacher
+    {
+       private string  no;
+       private string name;
+       private int years;
+            public string No
+        { get { return no; }
+          set { no = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Years
+        {
+            get { return years; }
+            set { years = value; }
+        }
+        public void Output()
+        {
+            Console.WriteLine("教师的工号是{0}",No);
+            Console.WriteLine("教师的年龄是{0}",Name);
+            Console.WriteLine("教师的工龄是{0}",Years);
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Teacher teacher= new Teacher();
+                Console.Write("请输入教师工号：");
+                teacher.No=Console.ReadLine();
+                Console.Write("请输入教师姓名：");
+                teacher.Name=Console.ReadLine();
+                Console.Write("请输入教师教龄：");
+                teacher.Years=int.Parse(Console.ReadLine());
+                Console.WriteLine("\n教师信息如下：");
+                teacher.Output();
+                Console.ReadKey();
+            }
+        }
+    }
+}
