e41da677ee8116906975c4850862527e0979a4d1
Moonlight-twj <2338144150@qq.com>
2025-10-21 09:36:17 +0800

    schoolwork

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public static int DaysDifferent(string dateS1, string dateS2, string format)
+    {
+        try
+        {
+            DateTime date1 = DateTime.ParseExact(dateS1, format, CultureInfo.InvariantCulture);
+            DateTime date2 = DateTime.ParseExact(dateS2, format, CultureInfo.InvariantCulture);
+            return Math.Abs((date2 - date1).Days);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("日期格式不正确，请使用{0}格式", format);
+            return -1;
+        }
+    }
+    public 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Teacher
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        int years;
+        public int Years
+        {
+            get { return years; }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("年龄不合法");
+                    years = 0;
+                }
+                else
+                    years = value;
+            }
+        }
+        public Teacher()
+        { }
+        public Teacher(string id)
+        {
+            ID = id;
+        }
+        public Teacher(string iD, string name)
+        {
+            ID= iD;
+            Name = name;
+        }
+        public Teacher(string id, string name, int years, int year)
+        {
+            ID = id;
+            Name = name;
+            Years = years;
+        }
+
+        public void OutPut()
+        {
+            Console.WriteLine("老师工号：{0}，姓名：{1}，教龄：{2}", ID, Name, Years);
+        }
+    }
+    class GouzaoMethod
+    {
+        static void Main(string[] args)
+        {
+            Teacher t12 = new Teacher();
+            t12.ID = "012";
+            t12.Name = "付心妍";
+            t12.Years = 3;
+            t12.OutPut();
+
+            Teacher t34=new Teacher("034");
+            t34.Name = "黄艳杰";
+            t34.Years = 4;
+            t34.OutPut();
+
+            Teacher t56 = new Teacher("056", "汤卡安");
+            t56.Years = 5;
+            t56.OutPut();
+        }
+    }
+}
